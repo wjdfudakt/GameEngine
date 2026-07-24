e02f5b83897519bdd1d0958647947f315fa5d8b1
wjdfudakt <xodus1227@naver.com>
2026-07-24 14:57:27 +0900

    260724
    유니티 프로그래밍 포트폴리오 과제

--- Assets/Script/Portfolio/Player/PlayerDeath.cs
+++ Assets/Script/Portfolio/Player/PlayerDeath.cs
@@ -1,17 +1,12 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour
 {
-    [Header("Game Over UI")]
-    [SerializeField] private GameObject gameOverPanel;
-
     private Health health;
 
     private void Awake()
     {
         health = GetComponent<Health>();
-
         if (health != null)
         {
             health.OnDeath.AddListener(OnDeath);
@@ -21,32 +16,8 @@ public class PlayerDeath : MonoBehaviour
     private void OnDeath()
     {
         Debug.Log("Player Dead");
-
         gameObject.SetActive(false);
 
-        if (gameOverPanel != null)
-        {
-            gameOverPanel.SetActive(true);
-        }
-
         Time.timeScale = 0f;
-    }
-
-    public void OnClickRestart()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    public void OnClickToTitle()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("TitleScene");
-    }
-
-    private void OnDestroy()
-    {
-        if (health != null)
-            health.OnDeath.RemoveListener(OnDeath);
-    }
+    }        
 }
